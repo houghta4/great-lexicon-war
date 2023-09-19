2d0d70a04cdf207557cde5810f5257df5a34fda4
Vince <houghta4@uwm.edu>
2023-09-19 11:23:28 -0500

    clear word system

--- src/game/word_match/mod.rs
+++ src/game/word_match/mod.rs
@@ -1,8 +1,7 @@
-use crate::game::word_match::systems::check_matches;
-use crate::AppState;
+use crate::game::word_match::systems::{check_matches, clear_word};
 use bevy::prelude::*;
 
-use super::InGameState;
+use super::InGameRunning;
 
 pub mod components;
 mod systems;
@@ -11,11 +10,6 @@ pub struct WordMatchPlugin;
 
 impl Plugin for WordMatchPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(
-            Update,
-            check_matches
-                .run_if(in_state(AppState::InGame))
-                .run_if(in_state(InGameState::Running)),
-        );
+        app.add_systems(Update, (check_matches, clear_word).in_set(InGameRunning));
     }
 }
